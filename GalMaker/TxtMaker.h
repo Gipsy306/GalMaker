73a834d4bee35c0b679fdb60036f26d5b074b46f
Gipsy306 <1105760290@qq.com>
2024-05-26 02:00:17 +0800

    some changes

---
+++ GalMaker/TxtMaker.h
@@ -0,0 +1,13 @@
+//该头文件用于实现将输入的指令转化为可以被 "showcase.h" 头文件下的类识别并处理的 ".txt" 文件。
+#pragma once
+#ifndef TXTMAKER_H
+#define TXTMAKER_H
+
+
+class FiletoTxt
+{
+private:
+public:
+};
+#endif // !TXTMAKER_H
+
