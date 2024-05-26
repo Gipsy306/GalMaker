6c3366849ab9a07086b8d76fae58f63f474d1fca
Creamraser <1572653178@qq.com>
2024-05-26 23:56:18 +0800

    王若水修改“ / \ s
    zA
    
    xas][
    /
    \\|
    
    I:'”

--- GalMaker/GalMaker.cpp
+++ GalMaker/GalMaker.cpp
@@ -10,6 +10,7 @@ int main()
 	cout << "Hello Git Bash!\n"<<endl;
 	cout << "Hello olden!\n" << endl;
 	cout << "Hello 123!" << endl;
+	cout << "Hello  456!" << endl;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
