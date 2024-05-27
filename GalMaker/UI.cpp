a8bfe222110d1b660158b44c2e5b614382c656d1
Gipsy306 <1105760290@qq.com>
2024-05-27 17:55:11 +0800

    some unfinished changes

--- GalMaker/UI.cpp
+++ GalMaker/UI.cpp
@@ -1,8 +1,8 @@
 #include"UI.h"
-
+constexpr auto err = 5;
 void ChooseBox::BoxPrint(ExMessage mouse)
 {
-	bool before = false,present=true;		//检查鼠标是否放在选择框上；
+	bool before = false, present = true;		//检查鼠标是否放在选择框上；
 	mouse = getmessage(EX_MOUSE | EX_KEY);
 	if ((mouse.x >= box_x) && (mouse.x <= box_x + length) && (mouse.y >= box_y) && (mouse.y <= box_y + width))
 	{
@@ -11,6 +11,21 @@ void ChooseBox::BoxPrint(ExMessage mouse)
 	}
 }
 
-void ChooseBox::BoxDraw(bool, bool)
+void ChooseBox::BoxDraw(bool before, bool present)
 {
+	//进行这样的操作以减少不断画图造成的性能损耗
+	if(before==present){}
+	else
+	{
+		//当鼠标进入选择框中时：
+		if (present)
+		{
+			/*具体绘制选择框的函数*/
+			setfillcolor(WHITE);
+			fillrectangle(box_x, box_y, box_x + length, box_y + width);
+			setfillcolor(BLUE);
+			fillrectangle(box_x, box_y, box_x + length, box_y + err);
+			fillrectangle(box_x, box_y, box_x + err, box_y + width);
+		}
+	}
 }
