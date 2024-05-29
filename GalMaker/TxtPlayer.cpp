3b71b0bf552b38880613dc1b415c00b24aa45f7f
Gipsy306 <1105760290@qq.com>
2024-05-29 23:52:24 +0800

    实现了输入，输出简单的文字模式

---
+++ GalMaker/TxtPlayer.cpp
@@ -0,0 +1,83 @@
+#include "TxtPlayer.h"
+
+void TxtPlay::TxtShowCase(const string& save_name,ExMessage mouse)
+{
+	string linereader;
+	string setsline;
+	ifstream in(save_name);
+	int count = 1;
+	/*while (getline(in, linereader))
+	{
+
+		if (mouse.vkcode == VK_RETURN)
+		{
+			if (count % 2 == 0)
+			{
+				
+			}
+			else
+			{
+				setsline_saver = linereader;
+			}
+			count++;
+		}
+		else if (mouse.vkcode == VK_ESCAPE)exit(0);
+		else {}
+		
+	}*/
+
+	
+	
+	while (true)
+	{
+		if (_kbhit())
+		{
+			int input = _getch();
+			if (input == '\r')
+			{
+				getline(in, setsline);
+				getline(in, linereader);
+				TypeSort(setsline, linereader);
+			}
+			else if (input == 27)
+			{
+				exit(0);
+			}
+		}
+	}
+}
+
+
+void TxtPlay::TypeSort(string sets, string contents)
+{
+	switch (stoi(sets.substr(0,2)))
+	{
+	case(1):Text_TxtPlay::getInstance()->TextShowcase(sets, contents); break;
+	default:break;
+	}
+}
+
+void Text_TxtPlay::TextShowcase(string sets, string contents)
+{
+	settextcolor(TextColor(sets));
+	outtextxy(0, 0, contents.data());
+}
+
+COLORREF Text_TxtPlay::TextColor(string sets)
+{
+	switch (stoi(sets.substr(2, 6)))
+	{
+	case(0):
+		return BLACK; break;
+	case(1):
+		return RED; break;
+	case(2):
+		return YELLOW; break;
+	case(3):
+		return BLUE; break;
+	case(4):
+		return GREEN; break;
+	default:break;
+	}
+}
+
---
+++ GalMaker/TxtPlayer.h
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef TXTPLAYER_H
+#define TXTPLAYER_H
+#include<fstream>
+#include<conio.h>
+#include"UI.h"
+
+
+class TxtPlay
+{
+private:
+	static TxtPlay* pTxtPlay;
+public:
+	static TxtPlay* getInstacne()
+	{
+		return pTxtPlay;
+	}
+	void TxtShowCase(const string& save_name,ExMessage mouse);
+	void TypeSort(string sets,string contents);
+};
+
+class Text_TxtPlay 
+{
+private:
+	static Text_TxtPlay* pText_TxtPlay;
+public:
+	static Text_TxtPlay* getInstance()
+	{
+		return pText_TxtPlay;
+	}
+	void TextShowcase(string sets,string contents);
+	COLORREF TextColor(string sets);
+};
+
+#endif // !TXTPLAYER_H
